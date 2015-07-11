7f5d4bf8c4f7fdf7d1e19dc36112ba236fa62245
Leon Mergen <leon@solatis.com>
2015-07-11 17:01:26 +0700

    Adds nix support

---
+++ bitcoin-block.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, base, binary, bitcoin-tx, bitcoin-types, bytestring
+, cryptohash, hexstring, hspec, largeword, lens, stdenv
+}:
+mkDerivation {
+  pname = "bitcoin-block";
+  version = "0.13.1";
+  src = ./.;
+  buildDepends = [
+    base binary bitcoin-tx bitcoin-types bytestring cryptohash
+    hexstring largeword lens
+  ];
+  testDepends = [
+    base bitcoin-tx bitcoin-types bytestring hexstring hspec
+  ];
+  homepage = "http://www.leonmergen.com/opensource.html";
+  description = "Utility functions for manipulating bitcoin blocks";
+  license = stdenv.lib.licenses.mit;
+}
